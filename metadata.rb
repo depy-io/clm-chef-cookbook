94f0941a478ad53273bd82e54623e50500d136ec
Liora Milbaum <liora@lioramilbaum.me>
2017-12-29 23:36:11 +0200

    Bumping to version 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'liora@lmb.co.il'
 license          'Apache v2.0'
 description      'IBM Collaborative Lifecycle Management'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 chef_version     '>= 12.1' if respond_to?(:chef_version)
 issues_url       'https://github.com/depy-io/clm-chef-cookbook/issues'
 source_url       'https://github.com/depy-io/clm-chef-cookbook'
